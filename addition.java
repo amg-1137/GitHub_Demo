c4e4b6203b2f4c2c76a79077ef33a6d312666ab2
amg-1137 <amogh.p@topgrep.com>
2023-03-11 08:56:03 +0530

    initial commit for new branch

--- addition.java
+++ addition.java
@@ -1,5 +1,6 @@
 public class addition {
     public static void main(String[] args) {
         System.out.println("This statement is to show addition.");
+        System.out.println("This statement is brand new.");
     }
 }
